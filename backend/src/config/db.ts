a56a303def5e4cf563dad7e870ed9f2e02083c51
Nourhene Ben Othmen <benothmennourhen8@gmail.com>
2026-07-18 23:29:48 +0100

    feat(backend): update db config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,3 +1,16 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const databaseUrl = process.env.NODE_ENV === 'test' && process.env.TEST_DATABASE_URL
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL;
+
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
+
